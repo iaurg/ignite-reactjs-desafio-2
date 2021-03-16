92368f5b4982d6d98cd0fe88bd948e3b1de897ad
Italo Aurelio <italoaurelior@gmail.com>
2021-03-16 19:46:22 -0300

    Finish challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+type Movie = {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+type ContentProps = {
+  selectedGenreTitle: string
+  movies: Movie[]
+}
+
+export function Content({movies, selectedGenreTitle} :ContentProps) {
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie:Movie) => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
